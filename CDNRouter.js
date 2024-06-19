0cc488ae03cfac209ec2f6a6aecb0834fd8cca46
AtheonAI <148398052+AetheonAI@users.noreply.github.com>
2024-06-20 11:21:09 +1200

    Update CDNRouter.js
    testt

--- CDNRouter.js
+++ CDNRouter.js
@@ -1,15 +1,6 @@
 (function() {
-    // Authentication logic goes here
-    // Example: Check the domain of the requesting website
-    var allowedDomains = ['accomify.com', 'www.accomify.com', 'file:///C:/Users/SeanD/OneDrive/Documents/Accomify/testsite.html'];
-    var currentDomain = window.location.hostname;
-    
-    if (allowedDomains.includes(currentDomain)) {
-      // Authentication successful, load the main chatbot script
-      var script = document.createElement('script');
-      script.src = 'https://d5umvjosg8xwo.cloudfront.net/AccomifyChatbot.js';
-      document.head.appendChild(script);
-    } else {
-      console.error('Unauthorized domain. Chatbot script not loaded.');
-    }
-  })();
+  // Load the main chatbot script for any domain
+  var script = document.createElement('script');
+  script.src = 'https://d5umvjosg8xwo.cloudfront.net/AccomifyChatbot.js';
+  document.head.appendChild(script);
+})();
