c5e89cf3be60a539d42e095147ddba520aa38663
AtheonAI <148398052+AetheonAI@users.noreply.github.com>
2024-06-20 16:40:36 +1200

    Update CDNRouter2.js

--- CDNRouter2.js
+++ CDNRouter2.js
@@ -1,6 +1,6 @@
 (function() {
   // Load the main chatbot script for any domain
   var script = document.createElement('script');
-  script.src = 'https://d5umvjosg8xwo.cloudfront.net/AccomifyChatbot.js';
+  script.src = 'https://d5umvjosg8xwo.cloudfront.net/AccomifyChatbot2.js';
   document.head.appendChild(script);
 })();
